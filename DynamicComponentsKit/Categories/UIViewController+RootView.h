c207d170918ed6954bcc63971626e00277a84a81
Radosław Szeja <rad3ks@gmail.com>
2014-08-14 23:49:58 +0200

    Added category for determing root/parent/top view in controller

---
+++ DynamicComponentsKit/Categories/UIViewController+RootView.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+RootView.h
+//  DynamicComponentsKit
+//
+//  Created by Radoslaw Szeja on 14.08.2014.
+//  Copyright (c) 2014 Radoslaw Szeja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (RootView)
+
+- (UIView *)topRootView;
+
+@end
